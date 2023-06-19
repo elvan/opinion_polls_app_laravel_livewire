df68be7e624567617399725e92b4b1494d60ec40
Elvan Hidayat <elvanhd@gmail.com>
2023-06-19 23:05:17 +0700

    creating a poll

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Poll;
+use App\Models\Vote;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +12,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Option extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
 
     public function poll(): BelongsTo
     {
--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Option;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -9,6 +10,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Poll extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title'];
 
     public function options(): HasMany
     {
